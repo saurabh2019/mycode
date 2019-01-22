909e5b2fb1be4af475820d37270b9f39c1b5cd00
Swadipt Saurabh <swadiptsaurabh@yahoo.co.in>
2019-01-22 16:57:16 -0500

    lab11-1

---
+++ dict01/pythondict02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#create a dictionary
+
+switch = {'hostname':'sw1','ip':'10.0.1.1','version':'1.2','vendor':'cisco'}
+
+#Display parts of the dictionary
+print (switch['hostname'])
+print (switch['ip'])
